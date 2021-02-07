9ea1f1387000663621a17408134e598403550c40
Eon-97 <77725825+Eon-97@users.noreply.github.com>
2021-02-07 19:18:38 +0900

    Add files via upload

---
+++ const.js
@@ -0,0 +1,10 @@
+// const를 사용하여 상수 선언
+
+const birthDay = '97.04.12';
+
+// 상수 재할당 오류
+/*
+birthDay = '2021.04.12';
+error, can't reassign the constant!
+*/
+
---
+++ variable.js
@@ -0,0 +1,20 @@
+// 변수 만들기
+let cat ;
+cat = '야옹야옹' ; //문자열 저장하기
+
+alert(cat);
+
+// 변수와 한당 값을 한 줄에 작성 
+let 구름 = '고양이';
+alert(구름)
+
+//한번에 여러 변수 선언 #forEach사용
+let 맥주 = '닭강정' , 와인 = '순대' ;
+[맥주,와인].forEach(alert)
+
+/*
+만들어진 지 오래된 스크립트에서는 let대신 var이라는 키워드를 사용했다
+
+변수를 두 번 선언하면 에러가 발생
+'message' has already been declared
+*/
